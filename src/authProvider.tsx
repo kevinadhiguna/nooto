509232bb3cf86c7e6d34ef357c113be437fc1f4e
kevinadhiguna <kevin26798@gmail.com>
2021-02-20 10:31:24 +0700

    refactor: wrap Component in AuthProvider

--- src/authProvider.tsx
+++ src/authProvider.tsx
@@ -1,3 +1,4 @@
+// authProvider.tsx provides data via context to rest of application
 import {
     useEffect,
     useState,
@@ -5,5 +6,34 @@ import {
     createContext,
     FunctionComponent
 } from 'react';
+import { firebase } from './initFirebase';
 
-export {};
+interface IContext {
+    user: firebase.User | null;
+    loading: boolean;
+    logout: () => void;
+}
+
+const AuthContext = createContext<IContext>({
+    user: null,
+    loading: true,
+    logout: () => {}
+});
+
+const AuthProvider: FunctionComponent = ({ children }) => {
+    const [user, setUser] = useState<firebase.User | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const cancelAuthListener = firebase.auth().onIdTokenChanged(u => {
+            setUser(u);
+            setLoading(false);
+        });
+
+        return () => cancelAuthListener();
+    }, []);
+
+    return <AuthContext.Provider value={{ user, loading, logout: () => firebase.auth().signOut() }}>{children}</AuthContext.Provider>
+}
+
+export { AuthProvider };
