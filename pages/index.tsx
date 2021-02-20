9adfe468035e60d832f94986c2e1e27099ea5725
kevinadhiguna <kevin26798@gmail.com>
2021-02-20 12:32:01 +0700

    refactor: add FirebaseAuth component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import FirebaseAuth from '../src/firebaseAuth';
 import { useAuth } from '../src/authProvider';
 
 export default function Home() {
   const { user, loading, logout } = useAuth();
 
   if (loading) return null;
-  if (!user) return <button className="link">Login</button>
+  if (!user) return <FirebaseAuth />;
 
   return(
     <main>
