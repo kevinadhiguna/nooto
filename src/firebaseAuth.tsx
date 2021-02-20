b7e8ccfb368136e667bb31b94a00b83920af454a
kevinadhiguna <kevin26798@gmail.com>
2021-02-20 10:43:23 +0700

    refactor: StyledFirebaseUI

--- src/firebaseAuth.tsx
+++ src/firebaseAuth.tsx
@@ -1,7 +1,6 @@
 import { FunctionComponent, useState, useEffect } from 'react';
-import StyledFirebaseAuth from 'react-firebaseui';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { firebase } from '../src/initFirebase';
 
 const firebaseAuthConfig = {
     signInFlow: 'popup',
@@ -16,3 +15,16 @@ const firebaseAuthConfig = {
     ],
     signInSuccessUrl: '/'
 };
+
+const FirebaseAuth: FunctionComponent = () => {
+    return(
+        <div>
+            <StyledFirebaseAuth 
+                uiConfig={firebaseAuthConfig} 
+                firebaseAuth={firebase.auth()}
+            />
+        </div>
+    );
+}
+
+export default FirebaseAuth;
